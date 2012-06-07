436f79ef7a7a637e53bd38ec28c38d70ccdeac1a
Carl Quinn <carl.quinn@gmail.com>
2012-06-07 16:49:27 -0700

    Fixed bad whitespace.

--- src/main/java/com/netflix/postreview/p4/P4Runner.java
+++ src/main/java/com/netflix/postreview/p4/P4Runner.java
@@ -11,7 +11,7 @@ public class P4Runner extends Runner {
     /**
      * Requires environment variables (P4USER, P4CLIENT, P4PORT, P4PASSWD)
      */
-    public P4Runner(String port, String client, String user, String passwd)  throws IOException {
+    public P4Runner(String port, String client, String user, String passwd) throws IOException {
         if (port != null) environment.put("P4PORT", port);
         if (client != null) environment.put("P4CLIENT", client);
         if (user != null) environment.put("P4USER", user);
